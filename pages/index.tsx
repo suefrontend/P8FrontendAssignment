ca01792864b7fc2d6b29d42b80ff2d5723a0df15
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-14 15:47:24 -0700

    Add container

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
+    <div className="container">
+      <h1>Hello world!</h1>
     </div>
   );
 };
