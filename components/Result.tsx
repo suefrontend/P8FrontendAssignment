aa7fc5a5f19d2e987a43758ad96e032e89210b34
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-16 02:39:55 -0700

    Add CSS animation to calculate result when loading

--- components/Result.tsx
+++ components/Result.tsx
@@ -11,7 +11,11 @@ function Result({ monthlyPayment, isLoading }: ResultProps) {
   return (
     <div className="result__container">
       <p className="result__text pt-14">Your total monthly payment will be</p>
-      <div className={`${isLoading && "opacity-30"} result__price font-black`}>
+      <div
+        className={`${
+          isLoading && "ct-animate-blink"
+        } result__price font-black`}
+      >
         {priceWithComma.split(".")[0]}
         <span className="result__decimal">
           {monthlyPayment.toString().split(".")[1]}
