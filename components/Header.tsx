38746e8645e778c2ee07e0e746fc2a05343b8733
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-17 02:39:59 -0700

    Quick fix for logo link

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,11 @@ function Header() {
   return (
     <header className="header">
       <div className="container flex items-center justify-between">
-        <div className="logo cursor-pointer">
-          <Link href="/" passHref>
+        <Link href="/" passHref>
+          <div className="logo cursor-pointer">
             <Image src="/p8Wordmark_sm.svg" layout="fill" alt="Produce8 logo" />
-          </Link>
-        </div>
+          </div>
+        </Link>
         <FiMenu size={30} className="text-grey cursor-pointer" />
       </div>
     </header>
