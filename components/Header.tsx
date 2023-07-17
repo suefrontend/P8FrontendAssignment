8c510814e86efcb435da4227e587139d8c3ce951
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-17 01:56:27 -0700

    Quick fix for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FiMenu } from "react-icons/fi";
 
 function Header() {
   return (
     <header className="header">
       <div className="container flex items-center justify-between">
-        <div className="logo">
-          <Image src="/p8Wordmark_sm.svg" layout="fill" alt="Produce8 logo" />
+        <div className="logo cursor-pointer">
+          <Link href="/" passHref>
+            <Image src="/p8Wordmark_sm.svg" layout="fill" alt="Produce8 logo" />
+          </Link>
         </div>
         <FiMenu size={30} className="text-grey cursor-pointer" />
       </div>
